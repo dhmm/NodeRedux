5918f75ebf8719621f67e96285e6ddcd2677d97b
unknown <dhmm@dhmm>
2019-07-12 16:29:15 +0300

    add modal added

--- front/frontfiles/index.js
+++ front/frontfiles/index.js
@@ -7,11 +7,9 @@ frontFiles.get('/' , (req,res) => {
 
 let assets  = '../../front/public/assets/';
 
-frontFiles.get('/materialize.min.css' , (req,res)=> { 
-  res.sendFile(path.join(__dirname , assets+'materialize/css/materialize.min.css'));
+
+frontFiles.get('/main.js' , (req,res)=> { 
+  res.sendFile(path.join(__dirname , assets+'js/main.js'));
 });
 
-frontFiles.get('/materialize.min.js' , (req,res)=> { 
-  res.sendFile(path.join(__dirname , assets+'materialize/js/materialize.min.js'));
-});
 module.exports = frontFiles;
---
+++ front/public/assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {           
+    $('#mdlAddNew').modal();
+    
+    $('#btnAddNew').click(function(){
+      $('#mdlAddNew').modal('open');
+    })
+
+})
+
+  
