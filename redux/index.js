29ac91e9e328cc8f8a5447fac4ff7d57fa1716be
unknown <dhmm@dhmm>
2019-07-15 11:52:16 +0300

    redux DRUC actions added

--- redux/index.js
+++ redux/index.js
@@ -1,9 +1,39 @@
 const store = require('./store');
 //action
-const { addTodo , changeTodo , deleteTodo , completeTodo , uncompleteTodo , getTodo , getAll , getCompleted , getUncompleted } = require('./actions');
+const { addTodo , changeTodo , deleteTodo , completeTodo , uncompleteTodo , getAll , getCompleted , getUncompleted } = require('./actions');
 
 const redux = {}
 
+redux.addTodo = (todo) => {
+  store.dispatch(addTodo(todo));
+  let reduxData = store.getState();
+  let todos = reduxData.todos;
+  return todos;
+}
+redux.changeTodo = (todo) => {
+  store.dispatch(changeTodo(todo));
+  let reduxData = store.getState();
+  let todos = reduxData.todos;
+  return todos;
+}
+redux.deleteTodo = (todo) => {
+  store.dispatch(deleteTodo(todo));
+  let reduxData = store.getState();
+  let todos = reduxData.todos;
+  return todos;
+}
+redux.completeTodo = (todoId) => {
+  store.dispatch(completeTodo(todoId));
+  let reduxData = store.getState();
+  let todos = reduxData.todos;
+  return todos;
+}
+redux.uncompleteTodo = (todoId) => {
+  store.dispatch(uncompleteTodo(todoId));
+  let reduxData = store.getState();
+  let todos = reduxData.todos;
+  return todos;
+}
 redux.getActive = () => {
   let reduxData = store.getState();
   let todos = reduxData.todos;
